8d91fbf5765f8ab60e99dd4a2a7a2b5933b70db6
saradejkoski <sara.dejkoski@stud.fh-campuswien.ac.at>
2022-01-24 19:47:15 +0100

    24.01.2022

--- src/main/java/at/ac/fhcampuswien/GUI.java
+++ src/main/java/at/ac/fhcampuswien/GUI.java
@@ -1,6 +1,6 @@
 package at.ac.fhcampuswien;
 
-import javax.swing.*; //shdfaöshfgasgf
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
