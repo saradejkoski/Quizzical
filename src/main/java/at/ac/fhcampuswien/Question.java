10eb365f925356a37a1db7af15a3a253cdf75c5e
Hanaa <Hanaa1311991>
2022-01-25 22:20:44 +0100

    Message

--- src/main/java/at/ac/fhcampuswien/Question.java
+++ src/main/java/at/ac/fhcampuswien/Question.java
@@ -1,9 +1,9 @@
 package at.ac.fhcampuswien;
 
 public class Question {
-    String[] answerOptions = new String[4]; //this array is responsible for the answer options
+    String[] answerOptions ; //this array is responsible for the answer options
     String question; //this string displays the question text
-    private int correctAnswer; //here is the correct answer (either 1-4);
+    private int correctAnswer; //here is the correct answer (either 0-3);
 
     public Question(String question, String[] answerOptions, int correctAnswer) {// alles in Parameter ist nur so lange da bis die Methode vorbei ist
         //es kommt erst die Frage, dann Antwortoptionen und dann die richtige Anwort
